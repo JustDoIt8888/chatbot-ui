bd9a7676fc0d13bd840869a5dfc8bc3366160ead
Elson Ho <elsonho.ws@gmail.com>
2023-09-10 23:27:20 +0800

    adding icon to chat message

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,4 +1,3 @@
-import { IconClearAll, IconSettings } from '@tabler/icons-react';
 import {
   MutableRefObject,
   memo,
@@ -28,11 +27,9 @@ import HomeContext from '@/pages/api/home/home.context';
 import Spinner from '../Spinner';
 import { ChatInput } from './ChatInput';
 import { ChatLoader } from './ChatLoader';
-import { ErrorMessageDiv } from './ErrorMessageDiv';
 import { MemoizedChatMessage } from './MemoizedChatMessage';
-import { ModelSelect } from './ModelSelect';
-import { SystemPrompt } from './SystemPrompt';
-import { TemperatureSlider } from './Temperature';
+
+// import { ErrorMessageDiv } from './ErrorMessageDiv';
 
 interface Props {
   stopConversationRef: MutableRefObject<boolean>;
@@ -332,120 +329,75 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
 
   return (
     <div className="relative flex-1 overflow-hidden bg-white dark:bg-[#343541]">
-      {!(apiKey || serverSideApiKeyIsSet) ? (
-        <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
-          <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
-          </div>
-          <div className="text-center text-lg text-black dark:text-white">
-            <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
-            <div className="mb-2 font-bold">
-              Important: Chatbot UI is 100% unaffiliated with OpenAI.
-            </div>
-          </div>
-          <div className="text-center text-gray-500 dark:text-gray-400">
-            <div className="mb-2">
-              Chatbot UI allows you to plug in your API key to use this UI with
-              their API.
-            </div>
-            <div className="mb-2">
-              It is <span className="italic">only</span> used to communicate
-              with their API.
-            </div>
-            <div className="mb-2">
-              {t(
-                'Please set your OpenAI API key in the bottom left of the sidebar.',
-              )}
-            </div>
-            <div>
-              {t("If you don't have an OpenAI API key, you can get one here: ")}
-              <a
-                href="https://platform.openai.com/account/api-keys"
-                target="_blank"
-                rel="noreferrer"
-                className="text-blue-500 hover:underline"
-              >
-                openai.com
-              </a>
-            </div>
-          </div>
-        </div>
-      ) : modelError ? (
-        <ErrorMessageDiv error={modelError} />
-      ) : (
-        <>
-          <div
-            className="max-h-full overflow-x-hidden"
-            ref={chatContainerRef}
-            onScroll={handleScroll}
-          >
-            {selectedConversation?.messages.length === 0 ? (
-              <>
-                <div className="mx-auto flex flex-col space-y-5 md:space-y-10 px-3 pt-5 md:pt-12 sm:max-w-[600px]">
-                  <div className="text-center text-3xl font-semibold text-gray-800 dark:text-gray-100">
-                    {models.length === 0 ? (
-                      <div>
-                        <Spinner size="16px" className="mx-auto" />
-                      </div>
-                    ) : (
-                      // ToDo: Display for new Conversation
-                      'Chatbot UI (New Conversation)'
-                    )}
-                  </div>
+      <>
+        <div
+          className="max-h-full overflow-x-hidden"
+          ref={chatContainerRef}
+          onScroll={handleScroll}
+        >
+          {selectedConversation?.messages.length === 0 ? (
+            <>
+              <div className="mx-auto flex flex-col space-y-5 md:space-y-10 px-3 pt-5 md:pt-12 sm:max-w-[600px]">
+                <div className="text-center text-3xl font-semibold text-gray-800 dark:text-gray-100">
+                  {models.length === 0 ? (
+                    <div>
+                      <Spinner size="16px" className="mx-auto" />
+                    </div>
+                  ) : (
+                    // ToDo: Change to some default title
+                    'Chatbot UI (New Conversation)'
+                  )}
                 </div>
-              </>
-            ) : (
-              <>
-                <div className="sticky top-0 z-10 flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
-                  {/**
-                   * // ToDo: display title of conversation
-                   * */}
-                  Placeholder for Title
-                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="sticky top-0 z-10 flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#444654] dark:text-neutral-200">
+                {selectedConversation?.name ?? 'New Conversation'}
+              </div>
 
-                {selectedConversation?.messages.map((message, index) => (
-                  <MemoizedChatMessage
-                    key={index}
-                    message={message}
-                    messageIndex={index}
-                    onEdit={(editedMessage) => {
-                      setCurrentMessage(editedMessage);
-                      // discard edited message and the ones that come after then resend
-                      handleSend(
-                        editedMessage,
-                        selectedConversation?.messages.length - index,
-                      );
-                    }}
-                  />
-                ))}
-
-                {loading && <ChatLoader />}
-
-                <div
-                  className="h-[162px] bg-white dark:bg-[#343541]"
-                  ref={messagesEndRef}
+              {selectedConversation?.messages.map((message, index) => (
+                <MemoizedChatMessage
+                  key={index}
+                  message={message}
+                  messageIndex={index}
+                  onEdit={(editedMessage) => {
+                    setCurrentMessage(editedMessage);
+                    // discard edited message and the ones that come after then resend
+                    handleSend(
+                      editedMessage,
+                      selectedConversation?.messages.length - index,
+                    );
+                  }}
                 />
-              </>
-            )}
-          </div>
+              ))}
 
-          <ChatInput
-            stopConversationRef={stopConversationRef}
-            textareaRef={textareaRef}
-            onSend={(message, plugin) => {
-              setCurrentMessage(message);
-              handleSend(message, 0, plugin);
-            }}
-            onScrollDownClick={handleScrollDown}
-            onRegenerate={() => {
-              if (currentMessage) {
-                handleSend(currentMessage, 2, null);
-              }
-            }}
-            showScrollDownButton={showScrollDownButton}
-          />
-        </>
-      )}
+              {loading && <ChatLoader />}
+
+              <div
+                className="h-[162px] bg-white dark:bg-[#343541]"
+                ref={messagesEndRef}
+              />
+            </>
+          )}
+        </div>
+
+        <ChatInput
+          stopConversationRef={stopConversationRef}
+          textareaRef={textareaRef}
+          onSend={(message, plugin) => {
+            setCurrentMessage(message);
+            handleSend(message, 0, plugin);
+          }}
+          onScrollDownClick={handleScrollDown}
+          onRegenerate={() => {
+            if (currentMessage) {
+              handleSend(currentMessage, 2, null);
+            }
+          }}
+          showScrollDownButton={showScrollDownButton}
+        />
+      </>
     </div>
   );
 });
